c888c9edb287f8f96e3325bade12cacf859d62cb
Rina Dz <dz.rina.na@gmail.com>
2022-11-30 03:03:21 +0300

    fix: исправить название классов

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-let editButton = document.querySelector('.profile__info_edit-button');
+let editButton = document.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
 let closeButton = document.querySelector('.popup__close-icon');
-let submitButton = document.querySelector('.popup__container_submit');
-let formElement = document.querySelector('.popup__container_text');
-let nameInput  = document.querySelector('.name_input');
-let jobInput  = document.querySelector('.job_input');
+let submitButton = document.querySelector('.popup__submit');
+let formElement = document.querySelector('.popup__text');
+let nameInput  = document.querySelector('.name__input');
+let jobInput  = document.querySelector('.job__input');
 
 function visiblePopup() {
     popup.classList.add('popup_opened');
@@ -19,8 +19,8 @@ closeButton.addEventListener('click', closePopup);
 
 function handleFormSubmit (evt) {
     evt.preventDefault(); 
-    let oldName = document.querySelector('.profile__info_name');
-    let oldJob = document.querySelector('.profile__info_description');
+    let oldName = document.querySelector('.profile__name');
+    let oldJob = document.querySelector('.profile__description');
     let newName = nameInput.value;
     let newJjob = jobInput.value;
 
